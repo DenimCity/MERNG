0279ee2abf79e3735e0d59d5f188db0eebcf5ccc
jxa8hst <jean_altidor@homdepot.com>
2019-07-06 15:30:11 -0400

    spacing

--- server/src/graphql/typeDef.js
+++ server/src/graphql/typeDef.js
@@ -11,17 +11,20 @@ module.exports = gql`
       likeCount: Int!
       commentCount: Int!
     }
+    
     type Comment {
       id: ID!
       createdAt: String!
       username: String!
       body: String!
     }
+
     type Like {
       id: ID!
       createdAt: String!
       username: String!
     }
+
     type User {
       id: ID!
       email: String!
@@ -29,16 +32,19 @@ module.exports = gql`
       username: String!
       createdAt: String!
     }
+
     input RegisterInput {
       username: String!
       password: String!
       confirmPassword: String!
       email: String!
     }
+
     type Query {
       getPosts: [Post]
       getPost(postId: ID!): Post
     }
+
     type Mutation {
       register(registerInput: RegisterInput): User!
       login(username: String!, password: String!): User!
@@ -48,6 +54,7 @@ module.exports = gql`
       deleteComment(postId: ID!, commentId: ID!): Post!
       likePost(postId: ID!): Post!
     }
+
     type Subscription {
       newPost: Post!
     }
