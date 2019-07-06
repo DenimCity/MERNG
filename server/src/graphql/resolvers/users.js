b1748439534e4b980d7589be417d4c60b453421f
jxa8hst <jean_altidor@homdepot.com>
2019-07-06 13:07:49 -0400

    user resolver working

---
+++ server/src/graphql/resolvers/users.js
@@ -0,0 +1,73 @@
+import User from '../../models/User';
+import bcrypt from 'bcryptjs';
+import { UserInputError } from 'apollo-server';
+import {validateRegisterInput, validateLoginInput} from '../../utils/validator';
+import { generateToken } from '../../utils/token';
+
+
+
+export default {
+      Mutation: {
+            async  login(_, {username, password }) {
+                  const {valid, errors} = validateLoginInput(username, password);
+
+                  if (!valid){
+                        throw new UserInputError("Errors", { errors})
+                  }
+
+                  const user = await User.findOne({username});
+
+                  if (!user) {
+                        errors.general = "User Not Found"
+                        throw new UserInputError('User Not Found', {errors})
+                  }
+
+                  const match = await bcrypt.compare(password, user.password)
+                  if (!match) {
+                        errors.general = "Wrong credentials"
+                        throw new UserInputError('Wrong credentials', {errors})
+                  }
+
+                  const token = generateToken(user)
+                   return {
+                        ...user._doc,
+                        id: user._id,
+                        token,
+                        createdAt: user.createdAt
+                  }
+            },
+            async register(_, { registerInput: { username, email, password, confirmPassword }}, context, info) {
+                  const {valid, errors} = validateRegisterInput(username, email, password, confirmPassword);
+                   if(!valid) {
+                         throw new UserInputError('Input Validation Error', {errors})
+                   }
+                  const user = await User.findOne({ username: username })
+                  if (user) {
+                        throw new UserInputError('Username is taken', {
+                              errors: {
+                                    username: 'This username is taken'
+                              }
+                        })
+                  }
+                  // Hash Password
+                  password = await bcrypt.hash(password, 12);
+                  const newUser = new User({
+                         email, 
+                        password,
+                        username,
+                        createdAt: new Date().toISOString()
+                  });
+
+                  const res = await newUser.save();
+
+                  const token = generateToken(res)
+
+                  return {
+                        ...res._doc,
+                        id: res._id,
+                        token,
+                        createdAt: res.createdAt
+                  }
+            }
+      }
+}
