e2a2ef3e5f132172688a0cb3fb8d7eb69365cd63
jxa8hst <jean_altidor@homdepot.com>
2019-07-06 13:08:22 -0400

    exported token generator

---
+++ server/src/utils/token/index.js
@@ -0,0 +1,10 @@
+import jwt from 'jsonwebtoken';
+
+export const generateToken = (user) => {
+     return  jwt.sign({
+            id: user.id,
+            email: user.email,
+            username: user.username
+      }, process.env.SECRET_KEY, {expiresIn: '1h' })
+
+}
