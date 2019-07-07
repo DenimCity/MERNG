5acf04daafbeec399d31878e87bcdec658d44d5f
jxa8hst <jean_altidor@homdepot.com>
2019-07-07 11:45:11 -0400

    global css

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import { ApolloClient } from "apollo-client";
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { createHttpLink } from 'apollo-link-http';
 import { ApolloProvider } from "@apollo/react-hooks";
-
+import './App.css'
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
