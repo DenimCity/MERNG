fe926be1e375559b04be7aec9c4ec804421543ab
jxa8hst <jean_altidor@homdepot.com>
2019-07-08 00:25:06 -0400

    add form validation to post form

--- client/src/components/pages/PostForm.js
+++ client/src/components/pages/PostForm.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Form, Button } from 'semantic-ui-react';
 import { useForm } from '../../utils/hooks';
 import gql from 'graphql-tag';
 
 import { useMutation} from '@apollo/react-hooks';
 import { FETCH_POSTS_QUERY } from '../../utils/graphql/queries';
+import FormErrors from '../FormErrors';
 
 const PostForm = () => {
 
+      const [errors, setErrors] =useState({})
 
       const { onChange,onSubmit, values } = useForm(createPost, {
             body: ''
@@ -24,10 +26,17 @@ const PostForm = () => {
       })
 
 function createPost(){
-      submitPost()
+      const errors = {}
+      if(values.body.trim() === '') {
+            errors.body = 'cannot be empty'
+            setErrors(errors)
+      } else {
+            submitPost()
+      }
+      
 }
       return (
-            
+            <div>
             <Form onSubmit={onSubmit}>
                   <Form.Field>
                         <Form.Input
@@ -35,11 +44,15 @@ function createPost(){
                               name="body"
                               onChange={onChange}
                               value={values.body}
-                              // error={}
+                              error={errors.body ? true : false}
                         />
                         <Button type="submit" color="teal"> Submit</Button>
                   </Form.Field>
             </Form>
+               {
+                  Object.keys(errors).length > 0 &&  <FormErrors errors={errors}/>
+                }
+            </div>
             
       )
 }
